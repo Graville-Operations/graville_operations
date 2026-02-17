d6484fae1051080770176ad1438dd95d8cc78219
Eunice Musembi <jonathaneunice752@gmail.com>
2026-02-17 21:10:28 +0300

    updated inventory screen

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-
-
       title: 'graville_operations',
       themeMode: ThemeMode.light,
       theme: ThemeData(
