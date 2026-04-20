397f42478bd5e1042ec1cb7e46c53eb09433d186
piexie3 <emmanuelkbett3@gmail.com>
2026-04-20 11:43:30 +0300

    fixed globalkey error

--- lib/core/routes/pages.dart
+++ lib/core/routes/pages.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:graville_operations/core/middleware/auth_middleware.dart';
-import 'package:graville_operations/models/dashboard/assign_user_screen.dart';
 import 'package:graville_operations/screens/admin/create_user_screen.dart';
 import 'package:graville_operations/screens/admin/users_list_screen.dart';
 import 'package:graville_operations/screens/application/binding.dart';
@@ -9,16 +8,13 @@ import 'package:graville_operations/screens/application/view.dart';
 import 'package:graville_operations/screens/auth/login/binding.dart';
 import 'package:graville_operations/screens/auth/login/view.dart';
 import 'package:graville_operations/screens/finance_dashboard/finance_dashboard.dart';
-import 'package:graville_operations/screens/projects/dashboard/assign_user_screen.dart';
 import 'package:graville_operations/screens/invoice/invoice_screen.dart';
 import 'package:graville_operations/screens/menus/menus.dart';
+import 'package:graville_operations/screens/projects/dashboard/assign_user_screen.dart';
 import 'package:graville_operations/screens/projects/dashboard/binding.dart';
 import 'package:graville_operations/screens/projects/dashboard/view.dart';
-import 'package:graville_operations/screens/sites/create_sites.dart';
 import 'package:graville_operations/screens/sites/create/view.dart';
-import 'package:graville_operations/screens/finance_dashboard/finance_dashboard.dart';
 
-import '../../screens/application/widgets/splash_screen.dart';
 import 'routes.dart';
 
 class AppPages {
@@ -28,36 +24,26 @@ class AppPages {
   static List<String> history = [];
   static final List<GetPage> routes = _rawRoutes
       .map((page) => GetPage(
-    name: page.name,
-    page: page.page,
-    binding: page.binding,
-    middlewares: [AuthMiddleware(priority: 0)],
-  ))
+            name: page.name,
+            page: page.page,
+            binding: page.binding,
+            middlewares: [AuthMiddleware(priority: 0)],
+          ))
       .toList();
 
   static final List<GetPage> _rawRoutes = [
     GetPage(
-      name: AppRoutes.login,
+      name: AppRoutes.initial,
       page: () => const LoginScreen(),
       binding: LoginBindings(),
     ),
     GetPage(
-        name: AppPages.application,
-        page: () => ApplicationScreen(),
-        binding: ApplicationBindings(),
-        middlewares: [AuthMiddleware(priority: 0)]),
-    GetPage(
-        name: AppRoutes.projectDashboard,
-        page: () => ProjectDashboardScreen(),
-        binding: ProjectDashboardBindings()),
-    GetPage(name: AppRoutes.createProject, page: () => CreateSitesScreen()),
+      name: AppPages.application,
+      page: () => ApplicationScreen(),
+      binding: ApplicationBindings(),
+    ),
     GetPage(
         name: AppRoutes.userDepartment, page: () => AssignUserToGroupScreen()),
-    //   name: AppPages.application,
-    //   page: () => ApplicationScreen(),
-    //   binding: ApplicationBindings(),
-    //   middlewares: [AuthMiddleware(priority: 0)],
-    // ),
     GetPage(
       name: AppRoutes.projectDashboard,
       page: () => ProjectDashboardScreen(),
@@ -67,7 +53,6 @@ class AppPages {
       name: AppRoutes.createProject,
       page: () => CreateSitesScreen(),
     ),
-    // Users menu routes
     GetPage(
       name: AppRoutes.usersDashboard,
       page: () => const UsersListScreen(),
@@ -80,9 +65,6 @@ class AppPages {
       name: AppRoutes.userRoles,
       page: () => const UsersListScreen(),
     ),
-    GetPage(name: AppRoutes.userDepartment, page: ()=>AssignUserToGroupScreen(),),
-
-    // ─── Finance menu routes
     GetPage(
       name: AppRoutes.financeDashboard,
       page: () => const FinanceDashboardApp(),
